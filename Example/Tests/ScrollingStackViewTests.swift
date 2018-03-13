5ace29c01f652a8caecdbd65e7257dfb2294212d
Jack Newcombe <jack.newcombe@just-eat.com>
2018-03-13 14:18:35 +0000

    Remove unneeded setup and tear down in unit tests

--- Example/Tests/ScrollingStackViewTests.swift
+++ Example/Tests/ScrollingStackViewTests.swift
@@ -4,19 +4,8 @@ import ScrollingStackViewController
 
 class ScrollingStackViewTests: XCTestCase {
     
-    
     let window = UIApplication.shared.keyWindow!
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
+
     func testSimpleScrolling() {
         
         // Sets up a scrolling stack view with two view controller with views both
