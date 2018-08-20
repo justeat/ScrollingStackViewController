1cdc9c34fbef4dad6bdc1640b62731df6650a86c
Arvindh <arvindh@tarkalabs.com>
2018-08-20 23:19:05 +0530

    Fixed view not removed from heirarchy on removal

--- ScrollingStackViewController/Classes/ScrollingStackViewController.swift
+++ ScrollingStackViewController/Classes/ScrollingStackViewController.swift
@@ -207,8 +207,8 @@ open class ScrollingStackViewController: UIViewController {
     
     open func remove(viewController: UIViewController) {
         guard let arrangedView = arrangedView(for: viewController) else { return }
-        stackView.removeArrangedSubview(arrangedView)
-        
+        arrangedView.removeFromSuperview()
+      
         viewController.willMove(toParentViewController: nil)
         viewController.removeFromParentViewController()
     }
