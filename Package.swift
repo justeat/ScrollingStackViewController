12dfc2cfe59b684f59c77f0b4421d5286c2ddc34
Dimitar Chakarov <dimitar.chakarov@just-eat.com>
2022-04-21 11:25:13 +0100

    Add support for SPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "ScrollingStackViewController",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "ScrollingStackViewController",
+            targets: ["ScrollingStackViewController"]),
+    ],
+    targets: [
+        .target(
+            name: "ScrollingStackViewController",
+            path: "ScrollingStackViewController/"
+        )
+    ]
+)
